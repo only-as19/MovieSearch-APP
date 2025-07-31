34be44a75543994eaa50c4d4fd12bf71c8d13b52
Asad <hellohello51981@gmail.com>
2025-07-31 22:38:40 +0500

    Movie Serach App - Create the simple header and style it

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import MovieSearch from "./components/searchComponent"
+
+const App = ()=>{
+  return(
+    <>
+    <main>
+      <div className="p-10 max-w-xl mx-auto uppercase">
+      <h1 className="text-center">React Movie Search </h1>
+      
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
