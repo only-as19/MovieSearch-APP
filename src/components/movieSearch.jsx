2317bf214965bb010404f7f938c86c619a2a167f
Asad <hellohello51981@gmail.com>
2025-07-31 22:55:57 +0500

    Movie Serach App - Style the search bar

--- src/components/movieSearch.jsx
+++ src/components/movieSearch.jsx
@@ -4,22 +4,22 @@ const MovieSearch = () => {
   return (
     <div>
       <form 
-           
+            className='grid text-2xl md:grid-cols-[auto_1fr_auto] md:items-center md:gap-4'
             
           >
             <label 
                 htmlFor="query"
-                
+                className='mb-3'
                 >Movie Name
             </label>
                 <input 
                 type="text" 
                 name='query' placeholder='i.e Jurassic Park' 
-                
+                className='border text-2xl py-3 px-5 rounded-3xl outline-none mb-2'
                 
                 />
             <button type='submit'
-                
+                className='border border-[rgba(0,0,0,0.75)] text-2xl py-3 px-5 rounded-3xl bg-[rgba(0,0,0,0.75)] text-white transition-all duration-300 hover:bg-[rgba(0,0,0,0.85)]'
                 >Search
             </button>
       </form>
