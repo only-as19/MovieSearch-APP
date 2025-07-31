979948a2493351312de1b3a69b2dff083ea85cbf
Asad <hellohello51981@gmail.com>
2025-07-31 22:58:10 +0500

    Movie Serach App - create a new function to handle the input

--- src/components/movieSearch.jsx
+++ src/components/movieSearch.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const MovieSearch = () => {
+
+    const[query,setQuery] = useState("")
+    const handleChange = (event)=>{
+    setQuery(event.target.value)
+  }
+
   return (
     <div>
       <form 
             className='grid text-2xl md:grid-cols-[auto_1fr_auto] md:items-center md:gap-4'
-            
+            onSubmit={searchMovies}
           >
             <label 
                 htmlFor="query"
@@ -16,7 +22,8 @@ const MovieSearch = () => {
                 type="text" 
                 name='query' placeholder='i.e Jurassic Park' 
                 className='border text-2xl py-3 px-5 rounded-3xl outline-none mb-2'
-                
+                value={query}
+                onChange={handleChange}
                 />
             <button type='submit'
                 className='border border-[rgba(0,0,0,0.75)] text-2xl py-3 px-5 rounded-3xl bg-[rgba(0,0,0,0.75)] text-white transition-all duration-300 hover:bg-[rgba(0,0,0,0.85)]'
