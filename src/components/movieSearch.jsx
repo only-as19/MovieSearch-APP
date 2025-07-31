05e1d02d34829930efbf14d0b9693c5464e75513
Asad <hellohello51981@gmail.com>
2025-07-31 22:54:19 +0500

    Movie Serach App - Add simple Search bar

--- src/components/movieSearch.jsx
+++ src/components/movieSearch.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 const MovieSearch = () => {
   return (
     <div>
-      
+      <form 
+           
+            
+          >
+            <label 
+                htmlFor="query"
+                
+                >Movie Name
+            </label>
+                <input 
+                type="text" 
+                name='query' placeholder='i.e Jurassic Park' 
+                
+                
+                />
+            <button type='submit'
+                
+                >Search
+            </button>
+      </form>
     </div>
   )
 }
